54caf4d2383d36468b1499e425ec0bd3e7a97e84
amsanders7 <asanders@winsupplyinc.com>
2022-09-22 13:15:08 -0400

    Refactoring to common crud interface. Closes #10.

--- pet-clinic-data/src/main/java/com/aliciaspringframework/mypetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/aliciaspringframework/mypetclinic/services/VetService.java
@@ -1,15 +1,7 @@
 package com.aliciaspringframework.mypetclinic.services;
 
-import com.aliciaspringframework.mypetclinic.model.Owner;
 import com.aliciaspringframework.mypetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
