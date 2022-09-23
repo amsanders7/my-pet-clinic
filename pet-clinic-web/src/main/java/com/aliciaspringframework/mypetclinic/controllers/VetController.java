4e2d199b88698209a0741c3d9a8469730ed2b442
amsanders7 <asanders@winsupplyinc.com>
2022-09-23 09:52:15 -0400

    Added vet list and updated controller. Closes #14.

--- pet-clinic-web/src/main/java/com/aliciaspringframework/mypetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/aliciaspringframework/mypetclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package com.aliciaspringframework.mypetclinic.controllers;
 
-import com.aliciaspringframework.mypetclinic.model.Vet;
+import com.aliciaspringframework.mypetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";  //path in project folder
     }
 }
