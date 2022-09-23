70a16b1a2770d92ca96ac3926539ebf8351815c3
amsanders7 <asanders@winsupplyinc.com>
2022-09-23 09:32:23 -0400

    Added owners list and updated controller. Closes #15.

--- pet-clinic-web/src/main/java/com/aliciaspringframework/mypetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/aliciaspringframework/mypetclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package com.aliciaspringframework.mypetclinic.controllers;
 
+import com.aliciaspringframework.mypetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    // no longer have to put autowired here -- it is done automatically by spring framework
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
