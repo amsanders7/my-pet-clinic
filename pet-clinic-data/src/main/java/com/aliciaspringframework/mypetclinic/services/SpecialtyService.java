8aed746a4ae86c9bb7668dc8cf2fe82370bbe621
amsanders7 <asanders@winsupplyinc.com>
2022-09-26 13:13:19 -0400

    Adding specialties to vets on startup. Closes #33.

--- pet-clinic-data/src/main/java/com/aliciaspringframework/mypetclinic/services/SpecialtyService.java
+++ pet-clinic-data/src/main/java/com/aliciaspringframework/mypetclinic/services/SpecialtyService.java
@@ -2,5 +2,5 @@ package com.aliciaspringframework.mypetclinic.services;
 
 import com.aliciaspringframework.mypetclinic.model.Specialty;
 
-public interface SpecialtiesService extends CrudService<Specialty, Long> {
+public interface SpecialtyService extends CrudService<Specialty, Long> {
 }
--- pet-clinic-data/src/main/java/com/aliciaspringframework/mypetclinic/services/map/SpecialtyMapService.java
+++ pet-clinic-data/src/main/java/com/aliciaspringframework/mypetclinic/services/map/SpecialtyMapService.java
@@ -1,11 +1,13 @@
 package com.aliciaspringframework.mypetclinic.services.map;
 
 import com.aliciaspringframework.mypetclinic.model.Specialty;
-import com.aliciaspringframework.mypetclinic.services.SpecialtiesService;
+import com.aliciaspringframework.mypetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
-public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
 
     @Override
     public Set<Specialty> findAll() {
