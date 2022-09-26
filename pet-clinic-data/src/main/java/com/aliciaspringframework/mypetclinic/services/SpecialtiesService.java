ab562b878d8d928a2a9cc6d063c1a81db8265504
amsanders7 <asanders@winsupplyinc.com>
2022-09-26 11:46:02 -0400

    Added Specialties Map Service Impl. Closes #30.

---
+++ pet-clinic-data/src/main/java/com/aliciaspringframework/mypetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.aliciaspringframework.mypetclinic.services;
+
+import com.aliciaspringframework.mypetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/aliciaspringframework/mypetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,34 @@
+package com.aliciaspringframework.mypetclinic.services.map;
+
+import com.aliciaspringframework.mypetclinic.model.Specialty;
+import com.aliciaspringframework.mypetclinic.services.SpecialtiesService;
+
+import java.util.Set;
+
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.deleteByObject(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
