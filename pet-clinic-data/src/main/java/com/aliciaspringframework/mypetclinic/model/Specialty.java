e8117cb50ec770510ee46bc1a7b87d3a5d6f15de
amsanders7 <asanders@winsupplyinc.com>
2022-09-26 10:28:10 -0400

    Added specialty model and associated it with vet model. Closes #26.

---
+++ pet-clinic-data/src/main/java/com/aliciaspringframework/mypetclinic/model/Specialty.java
@@ -0,0 +1,14 @@
+package com.aliciaspringframework.mypetclinic.model;
+
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
