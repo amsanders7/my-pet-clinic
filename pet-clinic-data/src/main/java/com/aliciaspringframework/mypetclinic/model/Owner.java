213344f252911ebe078c209475e3404f2ed9197b
amsanders7 <asanders@winsupplyinc.com>
2022-09-27 15:47:02 -0400

    Updated owner with JPA entity mappings. Closes #35.

--- pet-clinic-data/src/main/java/com/aliciaspringframework/mypetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/aliciaspringframework/mypetclinic/model/Owner.java
@@ -1,13 +1,23 @@
 package com.aliciaspringframework.mypetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")  //if owner is deleted, their pets will also be deleted
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
