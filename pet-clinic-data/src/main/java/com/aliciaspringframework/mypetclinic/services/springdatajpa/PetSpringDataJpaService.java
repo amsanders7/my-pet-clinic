91f2c38cc32dfbb9257ff76365795115fcbc10b8
amsanders7 <asanders@winsupplyinc.com>
2022-09-27 17:26:38 -0400

    Created spring data jpa service for pet model. Closes #42.

---
+++ pet-clinic-data/src/main/java/com/aliciaspringframework/mypetclinic/services/springdatajpa/PetSpringDataJpaService.java
@@ -0,0 +1,48 @@
+package com.aliciaspringframework.mypetclinic.services.springdatajpa;
+
+import com.aliciaspringframework.mypetclinic.models.Pet;
+import com.aliciaspringframework.mypetclinic.repositories.PetRepository;
+import com.aliciaspringframework.mypetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSpringDataJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSpringDataJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
