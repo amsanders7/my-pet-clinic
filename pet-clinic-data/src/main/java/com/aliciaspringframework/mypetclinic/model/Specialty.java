c4bbdc34e31dcbe62036c9f33110b31e1b95e2e1
amsanders7 <asanders@winsupplyinc.com>
2022-09-27 16:02:27 -0400

    Updated vet to use JPA. Closes #36.

--- pet-clinic-data/src/main/java/com/aliciaspringframework/mypetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/aliciaspringframework/mypetclinic/model/Specialty.java
@@ -1,9 +1,16 @@
 package com.aliciaspringframework.mypetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
 
     private String description;
 
+    @Column(name = "description")
     public String getDescription() {
         return description;
     }
