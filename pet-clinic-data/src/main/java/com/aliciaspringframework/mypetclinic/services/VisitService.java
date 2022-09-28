9249169edd90a71fd8a21fd4be509a8f2d50992a
amsanders7 <asanders@winsupplyinc.com>
2022-09-28 09:19:05 -0400

    Implemented map impl for visit service. Closes #47.

---
+++ pet-clinic-data/src/main/java/com/aliciaspringframework/mypetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.aliciaspringframework.mypetclinic.services;
+
+import com.aliciaspringframework.mypetclinic.models.Visit;
+import com.aliciaspringframework.mypetclinic.services.CrudService;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
