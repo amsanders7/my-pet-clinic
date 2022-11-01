785b13c21dd478f041f6cacb8d7f6548a565877a
amsanders7 <asanders@winsupplyinc.com>
2022-11-01 13:07:14 -0400

    Adding model attributes for Pet Controller. Closes #56

---
+++ pet-clinic-web/src/main/java/com/aliciaspringframework/mypetclinic/controllers/PetController.java
@@ -0,0 +1,45 @@
+package com.aliciaspringframework.mypetclinic.controllers;
+
+import com.aliciaspringframework.mypetclinic.models.Owner;
+import com.aliciaspringframework.mypetclinic.models.PetType;
+import com.aliciaspringframework.mypetclinic.services.OwnerService;
+import com.aliciaspringframework.mypetclinic.services.PetService;
+import com.aliciaspringframework.mypetclinic.services.PetTypeService;
+import java.util.Collection;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+}
