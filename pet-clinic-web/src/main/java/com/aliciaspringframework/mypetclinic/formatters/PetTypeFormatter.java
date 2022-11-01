fcbdba5d95f60dbc36fee883ca9c4e4d984c2587
amsanders7 <asanders@winsupplyinc.com>
2022-11-01 16:43:39 -0400

    Fixed parse issue on PetType. Closes #63.

---
+++ pet-clinic-web/src/main/java/com/aliciaspringframework/mypetclinic/formatters/PetTypeFormatter.java
@@ -0,0 +1,35 @@
+package com.aliciaspringframework.mypetclinic.formatters;
+
+import com.aliciaspringframework.mypetclinic.models.PetType;
+import com.aliciaspringframework.mypetclinic.services.PetTypeService;
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+
+    private final PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale) {
+        return petType.getName();
+    }
+
+    @Override
+    public PetType parse(String text, Locale locale) throws ParseException {
+        Collection<PetType> findPetTypes = petTypeService.findAll();
+        for (PetType type : findPetTypes) {
+            if (type.getName().equals(text)) {
+                return type;
+            }
+        }
+        throw new ParseException("type not found: " + text, 0);
+    }
+}
